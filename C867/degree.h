bc6ac46db073c2f942021ab81fa0e1dbb94fefde
DanielTierney <dannyjtierney@gmail.com>
2021-01-06 13:54:09 -0600

    Added repository

---
+++ C867/degree.h
@@ -0,0 +1,10 @@
+#pragma once
+
+/* Creating an enum for degree programs,
+	adding an UNKNOWN, other values will be checked
+	against it as we parse the roster */
+
+enum DegreeProgram { UNKNOWN, SECURITY, NETWORK, SOFTWARE };
+
+// For printing Degree Program strings, using DegreeProgram as index
+const std::string degreeProgramStrings[] = { "UNKNOWN", "SECURITY", "NETWORK", "SOFTWARE" };
